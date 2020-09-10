54e6ac92c9c67f7b54e6f17bdec7e1f1229fc9f7
orjuela45 <miguel.orjuela45@gmail.com>
2020-09-09 23:49:28 -0500

    gridview

---
+++ app/src/main/java/com/example/movil_taller1/FullImageActivity.java
@@ -0,0 +1,19 @@
+package com.example.movil_taller1;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+public class FullImageActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.image_details);
+        // Get intent data
+        Intent i = getIntent();
+        // Get Selected Image Id
+        int position = i.getExtras().getInt("id");
+        ImageAdapter imageAdapter = new ImageAdapter(this);
+        ImageView imageView = (ImageView) findViewById(R.id.full_image_view);
+        imageView.setImageResource(imageAdapter.thumbImages[position]);
+    }
+}
--- app/src/main/java/com/example/movil_taller1/GridViewExample.java
+++ app/src/main/java/com/example/movil_taller1/GridViewExample.java
@@ -1,8 +1,12 @@
 package com.example.movil_taller1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class GridViewExample extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class GridViewExample extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grid_view_example);
+        GridView gv = (GridView) findViewById(R.id.gvDetails);
+        gv.setAdapter(new ImageAdapter(this));
+        gv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                // Sending image id to FullScreenActivity
+                Intent i = new Intent(getApplicationContext(), FullImageActivity.class);
+                // passing array index
+                i.putExtra("id", position);
+                startActivity(i);
+            }
+        });
     }
 }
