ef3c2c8de676e1aa92d09fcfc428ad8114e0341e
ivanchuelo14 <ivanchuelo14@gmail.com>
2020-09-10 00:48:12 -0500

    Implementaicon Estilos y clases para tabHost

---
+++ app/src/main/java/com/example/movil_taller1/ContactActivity.java
@@ -0,0 +1,14 @@
+package com.example.movil_taller1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ContactActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact);
+    }
+}
